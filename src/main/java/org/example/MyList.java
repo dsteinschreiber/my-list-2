4914c605b114c3e6fba7e2f2fdf7303512f89a8c
dsteins <dsteinschreiber@gmail.com>
2022-04-22 17:11:45 -0500

    Added most basic methods to MyList interface
    Created class MyLinkedList

--- src/main/java/org/example/MyList.java
+++ src/main/java/org/example/MyList.java
@@ -1,4 +1,11 @@
 package org.example;
 
-public interface MyList {
+public interface MyList<T> extends Iterable<T>{
+    MyList<T> append(T element);
+    T first();
+    T last();
+    boolean isEmpty();
+    MyList<T> rest();
+    MyList<T> push(T element);
+
 }
