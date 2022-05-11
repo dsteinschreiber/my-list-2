837dd64438848bf6915439b2afd62789defeecdc
dsteins <dsteinschreiber@gmail.com>
2022-05-11 15:51:51 -0500

    Contains test for SortedList

--- src/test/java/org/example/MySortedListTest.java
+++ src/test/java/org/example/MySortedListTest.java
@@ -2,6 +2,9 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class MySortedListTest {
 
     @Test
@@ -9,4 +12,11 @@ public class MySortedListTest {
         System.out.println(MySortedList.of(3,2,1));
         System.out.println(MySortedList.of(9,4,12,55,9,2,0,3,-11));
     }
+
+    @Test
+    public void containsTest() {
+        assertTrue(MySortedList.of(9,4,12,55,9,2,0,3,-11).contains(9));
+        assertFalse(MySortedList.of(9,4,12,55,9,2,0,3,-11).contains(100));
+    }
+
 }
